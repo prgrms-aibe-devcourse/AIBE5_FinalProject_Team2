08c2a18db1648d43cc50ae6de65daccbc9a6096a
0cha0cha <taeju1013@gamil.com>
2026-05-27 09:29:18 +0900

    #4 feat: [frontend] 전역 ScrollToTop 컴포넌트 추가
    - 라우트 변경 시 window.scrollTo(0, 0) 실행하는 ScrollToTop 컴포넌트 추가
    - BrowserRouter 내부에 마운트하여 모든 페이지 전환에 적용

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import App from "./App.jsx";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
 import { LanguageProvider } from "./i18n/LanguageContext.jsx";
 import "./index.css";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary>
       <LanguageProvider>
         <BrowserRouter>
+          <ScrollToTop />
           <App />
         </BrowserRouter>
       </LanguageProvider>
